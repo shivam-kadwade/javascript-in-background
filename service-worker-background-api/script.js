4a27f973107193b6b479018f29d92b9450fe8f45
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2023-04-04 00:05:28 +0530

    Used more accurate timestamp using performance

--- service-worker-background-api/script.js
+++ service-worker-background-api/script.js
@@ -1,8 +1,12 @@
+let backgroundTimeStamp;
 document.addEventListener('visibilitychange',(event)=>{
     if(document.visibilityState === 'hidden'){
         document.getElementById('root').innerHTML = document.getElementById('root').innerHTML+ '<br/>'+`Going to background at ${new Date()}` + '<br/>'
+        backgroundTimeStamp= performance.now()
     }
     else{
         document.getElementById('root').innerHTML = document.getElementById('root').innerHTML+ `The page is visible  at ${new Date()}` + '<br/>'
+        timeElapsed = performance.now() - backgroundTimeStamp
+        console.log(`we are back from background after ${timeElapsed/1000}s`)
     }
 })
