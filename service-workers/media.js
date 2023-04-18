2b4d22348391fdc72a59adfb2d5a9d34c686475d
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2023-04-18 14:53:00 +0530

    Added event listener to assign metadata and play the audio

--- service-workers/media.js
+++ service-workers/media.js
@@ -1,1 +1,19 @@
 // media playing
+
+document.getElementById('btnPlay').addEventListener('click',()=>{
+    navigator.mediaSession.metadata =new MediaMetadata({
+        title: "Airbnb's official music",
+        artist: "Unknown",
+        album: "Airbnb",
+        artwork: [
+            {
+                src: "/media/airbnb.png",
+                type: "image/png",
+                sizes: [
+                    "800x800"
+                ]
+            }
+        ]
+    })
+    document.querySelector('audio').play()
+})
