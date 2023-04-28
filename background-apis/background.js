6a3e2b0026063d60b1884d9c35483cd046e92c2a
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2023-04-28 15:34:39 +0530

    Added event listener for background sync

--- background-apis/background.js
+++ background-apis/background.js
@@ -1,1 +1,10 @@
 navigator.serviceWorker.register("/sw.js")
+
+document.getElementById('btnBgSync').addEventListener('click',async ()=>{
+    if( 'SyncManager' in window){
+        const swReg = await navigator.serviceWorker.ready
+        console.log(swReg)
+        console.log(navigator)
+        swReg.sync.register('like')
+    }
+})
